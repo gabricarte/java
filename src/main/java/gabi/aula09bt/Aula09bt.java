a4c921b851bbf4384f271e7598acddb95bbc85ca
Gabriela Ricarte <gabrielagricarte1@gmail.com>
2022-09-07 16:58:22 -0300

    aula09

---
+++ src/main/java/gabi/aula09bt/Aula09bt.java
@@ -0,0 +1,25 @@
+
+package gabi.aula09bt;
+
+
+public class Aula09bt {
+
+    public static void main(String[] args) {
+        
+        Pessoa[] p = new Pessoa [2]; 
+        Livro[] l = new Livro[3];
+        
+        // Pessoas
+        p[0] = new Pessoa("Pedro", 22, "M"); 
+        p[1] = new Pessoa("Maria", 25, "F"); 
+        
+        //Livros 
+        l[0] = new Livro("Aprendendo Java", "José da Silva", 300, p[0]); 
+        l[1] = new Livro("POO Para Iniciantes", "Pedro Paulo", 500, p[1]); 
+        l[2] = new Livro("Java Avançado", "Maria Cândido", 800, p[0]); 
+        
+        l[0].abrir();
+        l[0].folhear(100);
+        System.out.println(l[0].detalhes());
+    }
+}

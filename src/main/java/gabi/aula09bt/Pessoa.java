e665c7658717fb86e4a57f9086a1082d459af4de
Gabriela Ricarte <gabrielagricarte1@gmail.com>
2022-09-07 17:00:30 -0300

    aula09

---
+++ src/main/java/gabi/aula09bt/Pessoa.java
@@ -0,0 +1,51 @@
+
+package gabi.aula09bt;
+
+public class Pessoa {
+     
+    //Atributos
+    
+    private String nome; 
+    private int idade; 
+    private String sexo; 
+    
+    // Métodos
+    
+    public void fazerAniver(){
+        this.idade++; // this.idade = this.idade + 1 
+    }
+    //Construtor
+
+    public Pessoa(String nome, int idade, String sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+ 
+    
+    //Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
+

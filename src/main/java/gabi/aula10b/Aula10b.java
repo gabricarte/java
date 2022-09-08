fe99b76e44417638644facaf24fd8ddc93a3f591
Gabriela Ricarte <gabrielagricarte1@gmail.com>
2022-09-08 14:57:13 -0300

    aula10

--- src/main/java/gabi/aula10b/Aula10b.java
+++ src/main/java/gabi/aula10b/Aula10b.java
@@ -5,11 +5,12 @@ package gabi.aula10b;
 public class Aula10b {
 
     public static void main(String[] args) {
+        
         Pessoa p1 = new Pessoa(); 
         Aluno p2 = new Aluno();
         Professor p3 = new Professor();
         Funcionario p4 = new Funcionario(); 
-        
+      
         p2.setNome("Pedro");
         p2.setIdade(15);
         p2.setSexo("M");
